25ddc615e8fa2d851f10bd9921a2039d754f7704
Jhon Santana <jhonsstn@gmail.com>
2026-09-22 19:07:10 -0300

    chore: delete the python application

--- python-app/tests/test_app_updates.py
+++
@@ -1,6 +0,0 @@
-from switch_catalog.app_updates import is_newer_version
-
-
-def test_is_newer_version_handles_v_tags():
-    assert is_newer_version("v0.2.0", "0.1.9")
-    assert not is_newer_version("v0.1.0", "0.1.0")
